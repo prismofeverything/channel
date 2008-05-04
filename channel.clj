751886880d2ea2e45bb2f1e4ecb8c11232ea927d
ooxwo <ooxwo@anazeentibot.lan>
2008-05-03 21:27:52 -0700

    added nernst equation

--- channel.clj
+++ channel.clj
@@ -4,10 +4,20 @@
 (defstruct ion :type :valence)
 (defstruct concentration :ion :level)
 
-(def ions (map (fn [spec] (struct :ion (spec 0) (spec 1))))
+(def ions 
+     (map (fn [spec] (struct :ion (spec 0) (spec 1))))
      [[:a -1]
       [:na 1]
       [:cl -1]
       [:k 1]
       [:ca 2]])
+
+(defn nernst
+  [membrane type]
+  (let [in (type (:inside membrane))
+	out (type (:outside membrane))
+	ratio (. Math (log (/ out in)))
+	valence (:valence (type ions))]
+    (* valence ratio)))
+    
 
