072ea72437809f5477764e3e47393e39db8719f0
ooxwo <ooxwo@anazeentibot.lan>
2008-05-03 20:22:18 -0700

    added valences for key ions

--- channel.clj
+++ channel.clj
@@ -1,6 +1,13 @@
 (defstruct plasm :contents :membranes :charge)
 (defstruct membrane :inside :outside :channels)
 (defstruct channel :permeability :activity :membrane)
-(defstruct ion :type :charge)
+(defstruct ion :type :valence)
 (defstruct concentration :ion :level)
+
+(def ions (map (fn [spec] (struct :ion (spec 0) (spec 1))))
+     [[:a -1]
+      [:na 1]
+      [:cl -1]
+      [:k 1]
+      [:ca 2]])
 
