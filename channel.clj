267e9144bc10888290c57a1a97da62448638f138
ooxwo <ooxwo@anazeentibot.lan>
2008-05-03 17:35:46 -0700

    two more structures

--- channel.clj
+++ channel.clj
@@ -1,4 +1,6 @@
 (defstruct plasm :contents :membranes :charge)
 (defstruct membrane :inside :outside :channels)
 (defstruct channel :permeability :activity :membrane)
+(defstruct ion :type :charge)
+(defstruct concentration :ion :level)
 
