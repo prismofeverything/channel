053828564191ab5f8ef2d331da9acdd1fb855511
ooxwo <ooxwo@anazeentibot.lan>
2008-05-04 17:56:12 -0700

    added level and membrane functions

--- channel.clj
+++ channel.clj
@@ -18,32 +18,67 @@
     (:valence ion)))
 
 (defn make-plasm
+  "a plasm maintains its concentrations as refs, as they will change often"
   [concentrations]
-  (struct plasm 
+  (struct plasm
 	  (let [contents (hash-map)]
 	    (reduce (fn [contents concentration] 
 			(assoc contents 
 			  (:type concentration) 
 			  (ref concentration)))
-		    content 
+		    contents
 		    concentrations))))
 
+(defn concentration-of
+  [plasm type]
+  (get (:concentrations plasm) type))
+
 (defn level-of
-  [type plasm]
-  (let [concentration (get (:concentrations plasm) type)]
-    (:level concentration)))
+  [plasm type]
+  (let [concentration (concentration-of plasm type)]
+    (:level @concentration)))
+
+(defn alter-level
+  [plasm type factor]
+  (sync nil
+    (let [concentration (concentration-of plasm type)]
+      (alter concentration assoc :level (+ factor (:level @concentration))))))
+
+(defn make-channel
+  [
+
+(defn make-membrane
+  [inside outside]
+  (struct membrane
+	  inside 
+	  outside 
+	  (ref [])))
+
+(defn add-channel
+  [membrane channel]
+  (sync nil
+    (let [embedded (assoc channel :membrane membrane)
+	  channels (:channels membrane)]
+      (alter channels cons embedded @channels))))
+
+(defn exchange
+  [membrane type factor]
+  (sync nil
+    (alter-level (:inside membrane) type factor)
+    (alter-level (:outside membrane) type (- factor))))
 
 (defn nernst
   [membrane type]
-  (let [in (type (:inside membrane))
-	out (type (:outside membrane))
+  (let [in (level-of (:inside membrane) type)
+	out (level-of (:outside membrane) type)
 	ratio (. Math (log (/ out in)))
 	valence (:valence (type ions))]
     (* valence ratio)))
-    
 
 
-  
+
+
+
 
 
 
